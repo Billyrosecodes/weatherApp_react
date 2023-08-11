fb6acbfdc94c354b6b0c616f63e19ba15dd886e6
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-11 23:55:11 +0100

    setting up the Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import axios from 'axios'; 
 
 function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e4fec9003589ca99adaccd20e23926ee`
   return (
-    <div className="App">
+    <div className="app">
+     <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>Dallas</p>
+        </div>
+        <div className="temp">
+          <h1>60F</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+      </div>
+     </div>
     </div>
   );
 }
