646e7c091c47e52a6ac761a449a867350f0763cf
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-12 03:44:54 +0100

    installing axios to handle Api requests and styling the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, {useState} from 'react';
-// import axios from 'axios'; 
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-
   // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e4fec9003589ca99adaccd20e23926ee`
   return (
     <div className="app">
-     <div className="container">
-      <div className="top">
-        <div className="location">
-          <p>Dallas</p>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
         </div>
-        <div className="temp">
-          <h1>60F</h1>
-        </div>
-        <div className="description">
-          <p>Clouds</p>
+        <div className="bottom">
+          <div className="feels">
+            <p>65F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">12MPH</div>
         </div>
       </div>
-     </div>
     </div>
   );
 }
